312db646d0b89326559487152fb3e0f6838ef3b4
chujitaozi <1484548330@qq.com>
2017-10-30 20:11:36 +0800

    主页基本完成,turtlebot的模板

--- test_site.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker build -t "turtlebot.com" .
-
-docker run -v `pwd`:/tmp/jekyll -w /tmp/jekyll -i -t --net=host turtlebot.com
-# docker run -v `pwd`:/tmp/jekyll -w /tmp/jekyll -i -t -p 0.0.0.0:4000:4000 turtlebot.com
